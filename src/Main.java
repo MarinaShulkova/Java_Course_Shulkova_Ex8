c9d9b31ce3f707c407856e43b85f353b31bd5255
Marash_79@mail.ru <MarinaShulkova>
2022-04-27 01:41:03 +0300

    Add occured

--- src/Main.java
+++ src/Main.java
@@ -32,19 +32,27 @@ public class Main {
         Scanner src = new Scanner(System.in);
         String symbol = src.nextLine();
         char[] ch = symbol.toCharArray();
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
         for (i = 0; i < ch.length; i++) {
             int count = 0;
             for (j = i + 1; j < ch.length; j++) {
-                if (ch[i] == ch[j]) {
+                if (ch[i] == ch[j] && j == i+1) {
                     count++;
+
+                    for (j = i + 2; j < ch.length; j++) {
+                        if (ch[i] == ch[j] && j==i+2) {
+                            count++;
+                        }
+
+                        if (count > 1) {
+                            System.out.print(symbol.charAt(i) + " Occured ");
+                            System.out.println();
+
+                        }
+                    }
                 }
             }
-            if (count != 0) {
-                System.out.print(symbol.charAt(i) + " Occured " + count + " time");
-                System.out.println();
-            }
-
         }
         return symbol;
     }
